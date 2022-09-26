34efa688c12c64ecf879b6f0cfa4e9b125d77168
Nathan Cochrane <79487879+RandomGuy3659@users.noreply.github.com>
2022-09-26 16:09:33 +1000

    Scorekeeper Script

---
+++ Week8PracVC/Assets/Scripts/Scorekeeper.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorekeeper : MonoBehaviour
+{
+    [SerializeField] private int coinPoints;
+    private int currentPoints;
+    public int CurrentPoints
+    {
+        get
+        {
+            return currentPoints;
+        }
+    }
+
+    static private Scorekeeper instance;
+    static public Scorekeeper Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                Debug.LogError("The instance is missing");
+            }
+            return instance;
+        }
+    }
+
+    void OnAwake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseScore()
+    {
+        currentPoints += coinPoints;
+    }
+}
